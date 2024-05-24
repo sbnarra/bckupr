9123d923fc1ccfb6d2e01f0da77788dcee8816d7
arran <vscode@server>
2024-05-24 23:35:28 +0000

    generated

---
+++ web/components/spec/models/index.ts
@@ -0,0 +1,13 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Backup';
+export * from './Filters';
+export * from './ModelError';
+export * from './Restore';
+export * from './Rotate';
+export * from './RotateInput';
+export * from './Status';
+export * from './StopModes';
+export * from './TaskInput';
+export * from './Version';
+export * from './Volume';
